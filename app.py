9843baf8e0339e52cf649fd8e2e79283bd55e0ee
Brian Ombati Agwora <132080922+Agwora@users.noreply.github.com>
2026-05-06 13:00:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def admin_cancel_booking(booking_id):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0',debug=True port=port)
+    app.run(host='0.0.0.0',debug=True, port=port)
