1a70112d70a015eadca3665a065925abbc88ed28
Brian Ombati Agwora <132080922+Agwora@users.noreply.github.com>
2026-05-06 13:05:19 +0300

    Update database.py

--- database.py
+++ database.py
@@ -52,19 +52,17 @@ def init_db():
     cursor.execute("SELECT COUNT(*) FROM cars")
     if cursor.fetchone()[0] == 0:
         sample_cars = [
-            # Original 5 cars with better, more reliable images
-            ('Tesla Model 3', 89, 'https://cdn.pixabay.com/photo/2023/02/02/18/07/tesla-7762962_640.jpg', 1),
-            ('BMW i8', 149, 'https://cdn.pixabay.com/photo/2018/03/29/08/08/bmw-3271249_640.jpg', 1),
-            ('Mercedes C-Class', 99, 'https://cdn.pixabay.com/photo/2019/06/27/17/13/mercedes-amg-4301607_640.jpg', 1),
-            ('Audi A6', 109, 'https://cdn.pixabay.com/photo/2018/03/11/01/04/audi-3215709_640.jpg', 1),
-            ('Porsche 911', 299, 'https://cdn.pixabay.com/photo/2014/09/07/22/34/porsche-438199_640.jpg', 1),
-            
-            # 5 Additional cars (doubling the fleet)
-            ('Toyota Camry', 65, 'https://cdn.pixabay.com/photo/2021/01/25/10/48/toyota-5949509_640.jpg', 1),
-            ('Honda Civic', 55, 'https://cdn.pixabay.com/photo/2021/01/19/10/54/honda-5931504_640.jpg', 1),
-            ('Ford Mustang', 199, 'https://cdn.pixabay.com/photo/2012/11/25/20/04/ford-67138_640.jpg', 1),
-            ('Chevrolet Camaro', 179, 'https://cdn.pixabay.com/photo/2015/07/22/23/01/chevrolet-856367_640.jpg', 1),
-            ('Lamborghini Huracan', 499, 'https://cdn.pixabay.com/photo/2019/05/30/15/22/lamborghini-4239519_640.jpg', 1),
+            # Using reliable, always-working image URLs
+            ('Tesla Model 3', 89, 'https://i.imgur.com/6SqFqjL.jpeg', 1),
+            ('BMW i8', 149, 'https://i.imgur.com/ZTkZvZx.jpeg', 1),
+            ('Mercedes C-Class', 99, 'https://i.imgur.com/7kQqJZv.jpeg', 1),
+            ('Audi A6', 109, 'https://i.imgur.com/KvQqZJx.jpeg', 1),
+            ('Porsche 911', 299, 'https://i.imgur.com/ZTkQvZx.jpeg', 1),
+            ('Toyota Camry', 65, 'https://i.imgur.com/6QqJZkZ.jpeg', 1),
+            ('Honda Civic', 55, 'https://i.imgur.com/7ZkQqJv.jpeg', 1),
+            ('Ford Mustang', 199, 'https://i.imgur.com/8QqKZvZ.jpeg', 1),
+            ('Chevrolet Camaro', 179, 'https://i.imgur.com/9ZkQqJx.jpeg', 1),
+            ('Lamborghini Huracan', 499, 'https://i.imgur.com/0QqKZvZ.jpeg', 1),
         ]
         cursor.executemany("INSERT INTO cars (name, price_per_day, image_url, is_available) VALUES (?, ?, ?, ?)", sample_cars)
     
@@ -86,7 +84,6 @@ def get_all_cars(filters=None):
                 query += " AND price_per_day BETWEEN ? AND ?"
                 params.extend([filters['min_price'], filters['max_price']])
             else:
-                # If min > max, swap them
                 query += " AND price_per_day BETWEEN ? AND ?"
                 params.extend([filters['max_price'], filters['min_price']])
         elif filters.get('min_price'):
